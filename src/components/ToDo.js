91544cbe02dfd7fdec1371711c1b931955a03ada
Delz <115114479+Delz-create@users.noreply.github.com>
2025-02-14 13:12:56 +0100

    Update ToDo.js

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { IoMdRemoveCircleOutline } from "react-icons/io";
 import { MdShare } from "react-icons/md";
 import { FaRegEdit } from "react-icons/fa";
